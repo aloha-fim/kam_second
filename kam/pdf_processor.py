81206c9ad41f741a75ffe16c54bad8b3569857a1
aloha-fim <frederec.im@gmail.com>
2024-09-18 01:03:48 +0200

    activate langchain

--- kam/pdf_processor.py
+++ kam/pdf_processor.py
@@ -147,7 +147,7 @@ def process_json_magic(query):
     #text = open("./data/postdefined_users_gpt.csv", "r")
 
     ##### Microsoft ######
-    with open("./data/postdefined_users_azure_data_v2.csv", encoding = "ISO-8859-1") as file:
+    with open("./data/postdefined_users_azure_data_v4.csv", encoding = "ISO-8859-1") as file:
         reader = csv.reader(file, delimiter=",")
         text_reader = list(reader)
 
